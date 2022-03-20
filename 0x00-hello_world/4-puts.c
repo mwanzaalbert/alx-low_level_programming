b11e56359efbcf360d45c6081f053774ecd236e5
mwanzaalbert <mwanzaalbert@gmail.com>
2022-03-20 05:59:50 -0700

    script 4-puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
 int main(void)
 {
 
-	char sentence[] = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(sentence);
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 
